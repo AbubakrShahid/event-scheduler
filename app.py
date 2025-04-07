e73ccf865c298d31571b423d6638471cd90922ab
AbubakrShahid <abubakarshahidd95@gmail.com>
2025-04-07 15:48:46 +0500

    Removed unused dependency

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask
-from flask_cors import CORS
 from routes.events import events_bp
 
 app = Flask(__name__)
-CORS(app)
 app.register_blueprint(events_bp)
 
 if __name__ == '__main__':
